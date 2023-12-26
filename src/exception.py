30f716f06f004961de74e0952b8bd8af407ab3a6
Artistarpit <arpittalmale4@gmail.com>
2023-12-26 20:29:19 +0530

    new commit

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,4 @@
-import sys
+import os, sys
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -19,6 +19,9 @@ class CustomeException(Exception):
         
         def __str__(self):
             return self.error_message
+        
+        def __repr__(self):
+            return CustomeException.__name__.str()
       
         
         
